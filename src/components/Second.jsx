a36f47515450764b3cfc573686a976a2c9249dee
liptonzuma <solomonboateng231@gmail.com>
2021-11-20 14:12:00 +0000

    list

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import { motion } from 'framer-motion/dist/framer-motion'
+import solution from '../solution.svg'
 
 function Second() {
     return (
         <motion.div className="mx-auto w-screen p-10">
-            <motion.div className="flex p-10">
-                <motion.div className="px-10 ">
-                    <motion.h1 className="sm:text-left sm:text-center pl-8 font-black sm:text-4xl md:text-5xl text-gray-800">
-                        Best Digital Markerting Solution.
+            <motion.div className="flex p-10 justify-center w-screen mx-auto">
+                {/* container */}
+                <motion.div className="px-10 md:w-1/2">
+                    {/* item 1 */}
+                    <motion.h1 className="text-5xl font-black text-gray-600">
+                        Best Digital Marketing Solutions.
                     </motion.h1>
-                <motion.div>
+                    {/* item1 description */}
+                    <motion.div>
+                        {/* content goes here */}
+                        <motion.p className="text-lg pt-5 text-gray-500">
+                            No matter what industry you’re in, we all live in a 
+                            more and more competitive world. It’s getting harder to stand
+                            out from the competition. That’s why, we decided to create a
+                            list of digital marketing solutions we believe every business 
+                            should implement as soon as possible unless they’ve already done it. 
+                        </motion.p>
+                    </motion.div>
+                </motion.div>
 
+                {/* Item 2 */}
+
+                <motion.div className="flex items-center justify-center mx-auto md:w-1/2">
+                    <img src={solution} alt="solutions" className="w-5/6"/>
                 </motion.div>
             </motion.div>
-
-            <motion.div>
-
-            </motion.div>
-
-            </motion.div>
-
 
         </motion.div>
     )
