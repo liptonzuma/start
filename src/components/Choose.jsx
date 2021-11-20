a722c27068f13d5ec69ab7b84c4ffa8ed7a16979
liptonzuma <solomonboateng231@gmail.com>
2021-11-20 18:29:58 +0000

    drop

--- src/components/Choose.jsx
+++ src/components/Choose.jsx
@@ -11,32 +11,58 @@ function Choose() {
                     <motion.img
                         initial={{opacity:0,x:100}}
                         whileInView={{opacity:1,x:0}}
-                        transition={{delay:.2,duration:.2}}
+                        transition={{delay:.2,duration:.3}}
                      src={choose} alt="solutions" className="w-5/6"/>
                     </motion.div>
 
                     <motion.div className="lg:w-1/2 ">
                         <motion.h1 className="md:text-5xl text-3xl lg:px-15  ">Why Choose Us</motion.h1>
-                        <motion.p className="text-gray-400 text-lg md:pr-10 pt-5">
+                        <motion.div className="text-gray-400 text-lg md:pr-10 pt-5">
                             There’s a certain set of steps that we believe should 
                             help you place yourself in the latter group.
-                        </motion.p>
+                        </motion.div>
                         <motion.div>
-                            <motion.p className="text-2xl wrapper p-3">
-                                <motion.i className=" fa fa-tasks text-2xl rounded-full bg-purple-700  icon text-white "></motion.i> <p className="pl-3">  Data Analysis</p>
-                            </motion.p>
+                            <motion.div 
+                            initial={{opacity:0,y:-10}}
+                            whileInView={{opacity:1,y:0}}
+                            transition={{duration:.6,delay:.6}}
+                            className="text-2xl wrapper p-3">
+                                <motion.i className=" fa fa-tasks text-2xl rounded-full bg-purple-700  icon text-white "></motion.i>
+                                 <div>
+                                 <p className="pl-3">Data Analysis</p>
+                                  <p className="text-base text-gray-400 pl-3">We wrangle and visualize your data and make predictions.
+                                  
+                                  </p>
+                                 </div>
+                            </motion.div>
                         </motion.div>
 
                         <motion.div>
-                            <motion.p className="text-2xl wrapper p-3">
-                                <motion.i className=" fa fa-pie-chart text-2xl rounded-full bg-purple-700  icon text-white "></motion.i> <p className="pl-3"> PPC Marketing</p>
-                            </motion.p>
+                            <motion.div 
+                            initial={{opacity:0,y:-10}}
+                            whileInView={{opacity:1,y:0}}
+                            transition={{duration:.6,delay:.9}}
+                            className="text-2xl wrapper p-3">
+                                <motion.i className=" fa fa-pie-chart text-2xl rounded-full bg-purple-700  icon text-white "></motion.i>
+                                 <div>
+                                 <p className="pl-3"> PPC Marketing</p>
+                                  <p className="text-base text-gray-400 pl-2">We make marketing very real to bring on new customers.</p>
+                                 </div>
+                            </motion.div>
                         </motion.div>
 
                         <motion.div>
-                            <motion.p className="text-2xl wrapper p-3">
-                                <motion.i className=" fa fa-newspaper-o text-2xl rounded-full bg-purple-700  icon text-white "></motion.i> <p className="pl-3"> Business Analytics</p>
-                            </motion.p>
+                            <motion.div 
+                            initial={{opacity:0,y:-10}}
+                            whileInView={{opacity:1,y:0}}
+                            transition={{duration:.6,delay:1.2}}
+                            className="text-2xl wrapper p-3">
+                                <motion.i className=" fa fa-newspaper-o text-2xl rounded-full bg-purple-700  icon text-white "></motion.i>
+                                 <div className="pl-3">
+                                  <p>Business Analytics</p>
+                                  <p className="text-base text-gray-400">We help you with your business statistics and analysis.</p>
+                                  </div>
+                            </motion.div>
                         </motion.div>
                     </motion.div>
             </motion.div>
