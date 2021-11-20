01c85b503c43ac0bc4b6b420860b4bd80314eb89
liptonzuma <solomonboateng231@gmail.com>
2021-11-20 23:32:03 +0000

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,8 @@ import { motion } from 'framer-motion/dist/framer-motion'
 function Features() {
     return (
        <motion.div>
-           
+           <motion.h1 className="md:text-center text-gray-500 text-left text-lg font-semibold">FEATURES</motion.h1>
+           <motion.h1 className="py-3 text-center text-5xl font-black text-gray-800">Our Special Features</motion.h1>
        </motion.div>
     )
 }
