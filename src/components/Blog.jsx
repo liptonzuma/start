5c00800835eb7a98d38baa83d4d779e6fcf11c2d
liptonzuma <solomonboateng231@gmail.com>
2021-11-23 14:15:01 +0000

    stories

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { motion } from 'framer-motion/dist/framer-motion'
+import image1 from '../assets/svg/xmas.svg'
 
 function Blog() {
     return (
-        <motion.div>
-            <p className="text-center text-lg text-gray-500 ">LATEST ARTICLES.</p>
-            <h1 className="text-4xl text-gray-700 text-center font-black">OUR STORIES BLOG</h1>
+        <motion.div className="p-32">
+            <p className="text-center text-lg">LATEST ARTICLES.</p>
+            <h1 className="text-5xl text-gray-700 text-center font-black py-3">Our Stories Posts</h1>
+            <motion.div className="bg-purple-500 w-3/12 h-60 justify-center rounded flex">
+                <motion.img 
+                    src={image1}
+                    alt="thumbnail"
+                    className="w-44"
+                />
+            </motion.div>
+            <motion.div>
+                    <p className="text-xl text-purple-700 py-2">Xmas Discounts</p>
+                </motion.div>
         </motion.div>
     )
 }
